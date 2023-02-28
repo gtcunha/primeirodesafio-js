9b056af71d86fcad3eea919c8ecc8469bbf32bec
gtcunha <gustavothebaldi@gmail.com>
2023-02-27 21:37:48 -0300

    Commit com a resolução do primeiro desafio de JS

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert("Testando o jS");
+
+//Montando a lista de teclas do telefone
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0 ; contador < listaDeTeclas.length; contador++) {
+       
+    const tecla = listaDeTeclas[contador];
+   
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+
+   
+      
+}
+
